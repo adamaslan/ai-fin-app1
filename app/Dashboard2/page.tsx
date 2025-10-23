a49f967cc70b7bb34f3500a14ba2c5f42c6f686a
adamaslan <chillcoders@gmail.com>
2025-10-23 19:37:48 -0400

    listofstonks2

--- app/Dashboard2/page.tsx
+++ app/Dashboard2/page.tsx
@@ -38,11 +38,7 @@ export const dynamic = "force-dynamic";
 export const runtime = "nodejs"; // must be Node.js, not Edge
 
 const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: {
-    client_email: process.env.GCP_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  },
+  projectId: process.env.GCP_PROJECT_ID
 });
 
 
