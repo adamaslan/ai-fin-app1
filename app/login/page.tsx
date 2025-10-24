64de08238ebc8f0b070f404c6f9b1cd09019172a
adamaslan <chillcoders@gmail.com>
2025-10-24 18:12:59 -0400

    clerk-redirect3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,10 @@ import { SignIn } from "@clerk/nextjs";
 export default function LoginPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-   <SignIn forceRedirectUrl="/Dashboard2" />
+   <SignIn forceRedirectUrl="/Dashboard2"  
+   
+   />
+   
     </div>
   )
 }
