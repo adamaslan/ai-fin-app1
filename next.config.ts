392a31b8354da7ea1c77ab7bdca4224ca94c2a94
adamaslan <chillcoders@gmal.com>
2025-12-05 22:04:42 -0500

    next-update3

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/lib': path.resolve(__dirname, './src/lib'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-    };
-    return config;
-  },
+  turbopack: {},
   /* other config options here */
 };
 
